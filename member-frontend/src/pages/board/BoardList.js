f36caf8b1f28dd5f2bdf80703950b8955134ed67
EunhoMoon <eunho.moon@gmail.com>
2022-02-16 17:51:11 +0900

    Board 구성

--- member-frontend/src/pages/board/BoardList.js
+++ member-frontend/src/pages/board/BoardList.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Table } from 'react-bootstrap';
+import Page from '../../components/Page';
 
 const BoardList = () => {
+  const pNum = 1;
+  const totalPage = 1;
+
   return (
-    <div>
-      <h1>게시판</h1>
+    <div className="container mt-2">
+      <Table striped bordered hover variant="dark">
+        <thead>
+          <tr>
+            <th width="10%">no</th>
+            <th width="40%">Title</th>
+            <th width="20%">Writer</th>
+            <th width="*">Upload Date</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* {users.map((user) => (
+            <UserItem user={user} pNum={pNum} key={user.id} />
+          ))} */}
+        </tbody>
+      </Table>
+
+      <Page pNum={pNum} totalPage={totalPage} />
     </div>
   );
 };
